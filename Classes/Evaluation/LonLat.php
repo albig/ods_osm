f32f8db72113fb1302cdb287d83afa677db25cc1
Alexander Bigga <alexander@bigga.de>
2024-11-19 19:51:14 +0100

    Trying fix eval latlon 3

--- Classes/Evaluation/LonLat.php
+++ Classes/Evaluation/LonLat.php
@@ -4,12 +4,12 @@ namespace Bobosch\OdsOsm\Evaluation;
 
 class LonLat
 {
-    public function evaluateFieldValue(string $value): string
+    public function evaluateFieldValue(string $value): ?string
     {
          // test if we have any value
          if ($value && $value !== '') {
             return sprintf('%01.6f', $value);
         }
-        return '';
+        return null;
     }
 }
